9f55e09ae0f2829a2e81db6fcddbfe3b8ac03db1
Nishant <nishantk03>
2021-01-02 16:38:41 +0530

    widget after question finish

--- lib/main.dart
+++ lib/main.dart
@@ -15,48 +15,52 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'question': 'What\'s your favourite color?',
+      'answer': ['Red', 'Organge', 'Green', 'Black']
+    },
+    {
+      'question': 'What\'s your favourite animal?',
+      'answer': ['Rabbit', 'Lion', 'Dog', 'Cow']
+    },
+    {
+      'question': 'What\'s your favourite food?',
+      'answer': ['Panner', 'Mashroom', 'Chicken', 'Noodles']
+    },
+  ];
+
   var _questionIndex = 0;
   void _answerChoosen() {
     setState(() {
-      if (_questionIndex < 2) {
-        _questionIndex++;
-      } else {
-        _questionIndex = 0;
-      }
+      _questionIndex++;
     });
-    print('Answer choosen!!!');
   }
 
   @override
   Widget build(BuildContext context) {
     //var questions = ['This is the 1st question?', 'This is the 2nd question?'];
-    var questions = [
-      {
-        'question': 'What\'s your favourite color?',
-        'answer': ['Red', 'Organge', 'Green', 'Black']
-      },
-      {
-        'question': 'What\'s your favourite animal?',
-        'answer': ['Rabbit', 'Lion', 'Dog', 'Cow']
-      },
-      {
-        'question': 'What\'s your favourite food?',
-        'answer': ['Panner', 'Mashroom', 'Chicken', 'Noodles']
-      },
-    ];
+
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
             title: Text('Quiz'),
           ),
-          body: Column(
-            children: [
-              Question(questions[_questionIndex]['question']),
-              ...(questions[_questionIndex]['answer'] as List<String>)
-                  .map((ans) => Answer(_answerChoosen, ans))
-                  .toList()
-            ],
-          )),
+          body: _questionIndex < questions.length
+              ? Column(
+                  children: [
+                    Question(questions[_questionIndex]['question']),
+                    ...(questions[_questionIndex]['answer'] as List<String>)
+                        .map((ans) => Answer(_answerChoosen, ans))
+                        .toList()
+                  ],
+                )
+              : Center(
+                  child: Text(
+                    'You did it !!!',
+                    style: TextStyle(fontSize: 40, color: Colors.blue),
+                  ),
+                )),
     );
   }
 }
